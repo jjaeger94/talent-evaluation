d7d36b1aaae856a79c7eccbbe5ec82d0301aea66
jjaeger94 <janjaeger2020@gmail.com>
2024-04-04 17:13:04 +0200

    use temp_db

--- public/class-talent-evaluation-public.php
+++ public/class-talent-evaluation-public.php
@@ -63,13 +63,20 @@ class Talent_Evaluation_Public {
 		if ( isset( $_POST['job_title'] ) ) {
 			$job_title = sanitize_text_field( $_POST['job_title'] );
 			$user_id = get_current_user_id(); // Nutzer-ID des anlegenden Nutzers
-	
-			// Datenbankverbindung herstellen
-			global $wpdb;
-			$table_name = $wpdb->prefix . 'jobs';
+
+			// Erfassen Sie die in den Optionen gespeicherten Daten
+			$db_host = get_option('te_db_host');
+			$db_name = get_option('te_db_name');
+			$db_user = get_option('te_db_user');
+			$db_password = get_option('te_db_password');
+		
+			// Versuchen Sie, eine temporäre Datenbankverbindung herzustellen
+			$temp_db = new wpdb($db_user, $db_password, $db_name, $db_host);
+
+			$table_name = $temp_db->prefix . 'jobs';
 	
 			// Neuen Eintrag in die Tabelle "Stellen" einfügen
-			$wpdb->insert( 
+			$temp_db->insert( 
 				$table_name, 
 				array( 
 					'user_id' => $user_id,
