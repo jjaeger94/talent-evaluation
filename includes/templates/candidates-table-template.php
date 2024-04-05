1dccf272de8d0c9e0025b87cc73393d4d6525e31
jjaeger94 <janjaeger2020@gmail.com>
2024-04-05 15:51:02 +0200

    nicer design

--- includes/templates/candidates-table-template.php
+++ includes/templates/candidates-table-template.php
@@ -1,15 +1,20 @@
 <form method="get">
-    <div class="form-group">
-        <label for="job_id">Stelle:</label>
-        <select class="form-control" id="job_id" name="job_id">
-            <option value="">Alle Stellen</option>
-            <?php foreach ( $jobs as $job ) : ?>
-                <option value="<?php echo esc_attr( $job->ID ); ?>" <?php selected( $selected_job, $job->ID ); ?>><?php echo esc_html( $job->job_title ); ?></option>
-            <?php endforeach; ?>
-        </select>
+    <div class="form-group row">
+        <div class="col-md-6">
+            <label for="job_id" class="col-form-label">Stelle:</label>
+            <select class="form-control" id="job_id" name="job_id">
+                <option value="">Alle Stellen</option>
+                <?php foreach ( $jobs as $job ) : ?>
+                    <option value="<?php echo esc_attr( $job->ID ); ?>" <?php selected( $selected_job, $job->ID ); ?>><?php echo esc_html( $job->job_title ); ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="col-md-2 align-self-end">
+            <button type="submit" class="btn btn-primary">Filtern</button>
+        </div>
     </div>
-    <button type="submit" class="btn btn-primary">Filtern</button>
 </form>
+
 <?php if ( ! empty( $candidates ) ) : ?>
     <div class="table-responsive">
         <table class="table table-striped">
