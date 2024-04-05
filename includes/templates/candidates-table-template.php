d607fd376729330eeab525907bd656aa48fb7cfa
jjaeger94 <janjaeger2020@gmail.com>
2024-04-05 15:40:44 +0200

    added dropdown to candidate table

--- includes/templates/candidates-table-template.php
+++ includes/templates/candidates-table-template.php
@@ -1,3 +1,15 @@
+<form method="get">
+    <div class="form-group">
+        <label for="job_id">Stelle:</label>
+        <select class="form-control" id="job_id" name="job_id">
+            <option value="">Alle Stellen</option>
+            <?php foreach ( $jobs as $job ) : ?>
+                <option value="<?php echo esc_attr( $job->ID ); ?>" <?php selected( $selected_job, $job->ID ); ?>><?php echo esc_html( $job->job_title ); ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary">Filtern</button>
+</form>
 <?php if ( ! empty( $candidates ) ) : ?>
     <div class="table-responsive">
         <table class="table table-striped">
