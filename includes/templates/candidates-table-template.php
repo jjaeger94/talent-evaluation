8d6aa14f20a106a063aa3617cb1293f047e79356
jjaeger94 <janjaeger2020@gmail.com>
2024-04-05 15:15:59 +0200

    make table nicer

--- includes/templates/candidates-table-template.php
+++ includes/templates/candidates-table-template.php
@@ -1,20 +1,22 @@
 <?php if ( ! empty( $candidates ) ) : ?>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Kandidat</th>
-                <th>Stelle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ( $candidates as $candidate ) : ?>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td><?php echo esc_html( $candidate->prename . ' ' . $candidate->surname ); ?></td>
-                    <td><?php echo esc_html( $candidate->job_title ); ?></td>
+                    <th>Kandidat</th>
+                    <th>Stelle</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ( $candidates as $candidate ) : ?>
+                    <tr>
+                        <td><?php echo esc_html( $candidate->prename . ' ' . $candidate->surname ); ?></td>
+                        <td><?php echo esc_html( $candidate->job_title ); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 <?php else : ?>
     <p>Keine Kandidaten gefunden.</p>
 <?php endif; ?>
