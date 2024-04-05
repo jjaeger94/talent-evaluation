3d8eb12adda1cd7bf8a4c7ca89471111344d79d4
jjaeger94 <janjaeger2020@gmail.com>
2024-04-05 10:09:16 +0200

    added fields to job form

--- includes/forms/job-form.php
+++ includes/forms/job-form.php
@@ -1,10 +1,43 @@
-<form id="add-job-form" class="bootstrap-form">
+<form id="add-job-form" class="bootstrap-form" method="post">
     <div class="form-group">
         <label for="job-title">Stellenbezeichnung:</label>
         <input type="text" class="form-control" id="job-title" name="job_title" required>
     </div>
+    <div class="form-group">
+        <label for="criteria1">Kriterien zur Vorauswahl:</label>
+        <input type="text" class="form-control" id="criteria1" name="criteria1">
+        <input type="text" class="form-control" id="criteria2" name="criteria2">
+        <input type="text" class="form-control" id="criteria3" name="criteria3">
+    </div>
+    <div class="form-group">
+        <label>Vollständigkeits Check</label>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="completeness1" name="completeness1" value="0">
+            <label class="form-check-label" for="completeness1">Zeugnisse auf Vollständigkeit prüfen</label>
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="completeness2" name="completeness2" value="0">
+            <label class="form-check-label" for="completeness2">Arbeitszeugnisse auf Vollständigkeit prüfen</label>
+        </div>
+    </div>
+    <div class="form-group">
+        <label>Referenz Check</label>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="reference1" name="reference1" value="0">
+            <label class="form-check-label" for="reference1">LinkedIn checken</label>
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="reference2" name="reference2" value="0">
+            <label class="form-check-label" for="reference2">Höchstes Bildungszeugnis prüfen</label>
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="reference3" name="reference3" value="0">
+            <label class="form-check-label" for="reference3">Arbeitszeugnis prüfen</label>
+        </div>
+    </div>
     <input type="submit" value="Stelle hinzufügen" class="btn btn-primary">
 </form>
+
 
 <!-- Container für Fehler- oder Erfolgsmeldungen -->
 <div id="form-message"></div>
