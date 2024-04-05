b96eff99c0e97c448db94279558ad2d36832fa7f
jjaeger94 <janjaeger2020@gmail.com>
2024-04-05 10:12:05 +0200

    added margins

--- includes/forms/job-form.php
+++ includes/forms/job-form.php
@@ -1,15 +1,15 @@
 <form id="add-job-form" class="bootstrap-form" method="post">
-    <div class="form-group">
+    <div class="form-group mb-3">
         <label for="job-title">Stellenbezeichnung:</label>
         <input type="text" class="form-control" id="job-title" name="job_title" required>
     </div>
-    <div class="form-group">
+    <div class="form-group mb-3">
         <label for="criteria1">Kriterien zur Vorauswahl:</label>
         <input type="text" class="form-control" id="criteria1" name="criteria1">
-        <input type="text" class="form-control" id="criteria2" name="criteria2">
-        <input type="text" class="form-control" id="criteria3" name="criteria3">
+        <input type="text" class="form-control mt-2" id="criteria2" name="criteria2">
+        <input type="text" class="form-control mt-2" id="criteria3" name="criteria3">
     </div>
-    <div class="form-group">
+    <div class="form-group mb-3">
         <label>Vollständigkeits Check</label>
         <div class="form-check">
             <input type="checkbox" class="form-check-input" id="completeness1" name="completeness1" value="0">
@@ -20,7 +20,7 @@
             <label class="form-check-label" for="completeness2">Arbeitszeugnisse auf Vollständigkeit prüfen</label>
         </div>
     </div>
-    <div class="form-group">
+    <div class="form-group mb-3">
         <label>Referenz Check</label>
         <div class="form-check">
             <input type="checkbox" class="form-check-input" id="reference1" name="reference1" value="0">
@@ -38,6 +38,5 @@
     <input type="submit" value="Stelle hinzufügen" class="btn btn-primary">
 </form>
 
-
 <!-- Container für Fehler- oder Erfolgsmeldungen -->
-<div id="form-message"></div>
+<div id="form-message" class="mt-3"></div>
