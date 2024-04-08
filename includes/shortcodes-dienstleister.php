1e4201fa43f0a99f8f9d9ad002c75ff9f1067928
jjaeger94 <janjaeger2020@gmail.com>
2024-04-08 09:56:51 +0200

    delete unused shortcodes

--- includes/shortcodes-dienstleister.php
+++ includes/shortcodes-dienstleister.php
@@ -3,21 +3,8 @@
      * Hier alle Shortcodes für Dienstleister eintragen!
      */
     function register_shortcodes_dienstleister() {
-        add_shortcode('dienstleister_page', 'dienstleister_page_shortcode');
         add_shortcode( 'show_tasks', 'show_tasks_table' );
     }
-
-    /**
-     * Shortcode-Callback für die Dienstleister-Seite
-     *
-     * @param array $atts Array von Attributen, die im Shortcode verwendet werden können.
-     * @param string $content Der Inhalt innerhalb des Shortcodes, wenn der Shortcode als Paar verwendet wird.
-     * @return string Der HTML-Inhalt der Firmenkunden-Seite.
-     */
-    function dienstleister_page_shortcode($atts, $content = null) {
-        // Hier den Inhalt der Firmenkunden-Seite einfügen
-        return "Hier können Dienstleister Bewerber auflisten und Scores vergeben.";
-}
 
     // Kurzer Shortcode zum Anzeigen der Kandidatentabelle
     function show_tasks_table() {
--- includes/shortcodes-firmenkunden.php
+++ includes/shortcodes-firmenkunden.php
@@ -3,26 +3,12 @@
      * Hier alle Shortcodes für Firmenkunden eintragen!
      */
     function register_shortcodes_firmenkunden() {
-        add_shortcode('firmenkunden_page', 'firmenkunden_page_shortcode');
         add_shortcode( 'add_job_form', 'add_job_form_shortcode' );
         add_shortcode( 'show_jobs', 'show_jobs_table' );
         add_shortcode( 'create_candidate_form', 'render_create_candidate_form' );
         add_shortcode( 'show_candidates', 'show_candidates_table' );
     }
     
-
-    /**
-     * Shortcode-Callback für die Firmenkunden-Seite
-     *
-     * @param array $atts Array von Attributen, die im Shortcode verwendet werden können.
-     * @param string $content Der Inhalt innerhalb des Shortcodes, wenn der Shortcode als Paar verwendet wird.
-     * @return string Der HTML-Inhalt der Dienstleister-Seite.
-     */
-    function firmenkunden_page_shortcode($atts, $content = null) {
-        // Hier den Inhalt der Dienstleister-Seite einfügen
-        return "Hier können Firmenkunden Bewerber hinzufügen und Dokumente hochladen.";
-    }
-
     /**
      * 
      */
