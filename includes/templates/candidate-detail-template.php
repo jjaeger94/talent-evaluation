9e562a21f97761e323d58de9725291dcafe7dd14
jjaeger94 <janjaeger2020@gmail.com>
2024-04-08 15:17:42 +0200

    added progress bar

--- includes/templates/candidate-detail-template.php
+++ includes/templates/candidate-detail-template.php
@@ -1,18 +1,18 @@
 <?php if ( $application ) : ?>
     <div class="candidate-details">
         <h2><?php echo esc_html( $application->prename . ' ' . $application->surname ); ?></h2>
-        <p><strong>E-Mail:</strong> <?php echo esc_html( $application->email ); ?></p>
+        <p><?php echo esc_html( $application->email ); ?></p>
         <hr>
         <div class="row">
             <div class="col-md-6">
-                <p><strong>Beworben auf:</strong> <?php echo esc_html( $job->job_title ); ?></p>
+                <p><strong>Beworben auf:</strong><br><?php echo esc_html( $job->job_title ); ?></p>
             </div>
             <div class="col-md-6">
                 <p><strong>Einordnung:</strong>
-                    <select class="form-control">
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
+                    <select class="form-control" id="classification" name="classification">
+                        <option value="0">Automatische Einordnung</option>
+                        <option value="1">Manuell positiv</option>
+                        <option value="2">Manuell negativ</option>
                     </select>
                 </p>
             </div>
@@ -20,13 +20,12 @@
         <hr>
         <p><strong>Status der Prüfung:</strong></p>
         <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Kriterien werden überprüft</div>
-            <div class="progress-bar bg-info" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Vollständigkeit wird sichergestellt</div>
-            <div class="progress-bar bg-warning" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Referenzen werden überprüft</div>
-            <div class="progress-bar bg-secondary" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Prüfung abgeschlossen</div>
+            <div class="progress-bar overflow-visible" role="progressbar" style="width: <?php echo esc_attr( ($active_status_index + 1) * 25 ); ?>%;" aria-valuenow="<?php echo esc_attr( $active_status_index + 1 ); ?>" aria-valuemin="1" aria-valuemax="4"><?php echo esc_html( $statuses[$active_status_index] ); ?></div>
         </div>
+        <br>
+        <p><strong>Ergebnis der Prüfung:</strong><br><?php echo esc_html( $prüfungsergebnis ); ?></p>
         <hr>
-        <p><strong>Ergebnis der Prüfung:</strong> <?php echo esc_html( $prüfungsergebnis ); ?></p>
+        <p><strong>Ergebnis Commitment Test:</strong><br><?php echo esc_html( $prüfungsergebnis ); ?></p>
     </div>
 <?php else : ?>
     <div class="alert alert-warning" role="alert">Es wurden keine Bewerbungsdetails gefunden.</div>
