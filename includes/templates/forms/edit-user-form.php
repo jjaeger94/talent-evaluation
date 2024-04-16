2a9768425b0954187ad85e93c151587e5714fc23
jjaeger94 <janjaeger2020@gmail.com>
2024-04-16 11:07:20 +0200

    make edit user form less ugly

--- includes/templates/forms/edit-user-form.php
+++ includes/templates/forms/edit-user-form.php
@@ -1,14 +1,14 @@
 <form id="edit-user-data-form" method="post">
-    <div class="form-group">
-        <label for="first-name">Vorname</label>
+    <div class="form-group mb-3">
+        <label for="first-name"><strong>Vorname</strong></label>
         <input type="text" class="form-control" id="first-name" name="first_name" value="<?php echo esc_attr(get_user_meta($user_id,'first_name', true)); ?>" required>
     </div>
-    <div class="form-group">
-        <label for="last-name">Nachname</label>
+    <div class="form-group mb-3">
+        <label for="last-name"><strong>Nachname</strong></label>
         <input type="text" class="form-control" id="last-name" name="last_name" value="<?php echo esc_attr(get_user_meta($user_id,'last_name', true)); ?>" required>
     </div>
-    <div class="form-group">
-        <label for="company">Firma</label>
+    <div class="form-group mb-3">
+        <label for="company"><strong>Firma</strong></label>
         <input type="text" class="form-control" id="company" name="company" value="<?php echo esc_attr(get_user_meta($user_id,'company', true)); ?>" required>
     </div>
     <br>
