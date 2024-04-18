9422358d5fdc7c1370891901ee51a1c8ad5b5dec
jjaeger94 <janjaeger2020@gmail.com>
2024-04-18 14:25:54 +0200

    fix link for consent

--- public/partials/consent-mail.php
+++ public/partials/consent-mail.php
@@ -2,5 +2,5 @@
 <p>Schön das du dich auf die Stelle <?php echo $job->jobname; ?> beworben hast.</p>
 <p>Wir von Commit IQ überprüfen für <?php echo get_user_meta($job->user_id, 'company', true); ?> einige deiner Bewerberdaten und benötigen dein Einverständnis.</p>
 <p>Bitte klicke auf den unten stehenden Link, um uns dein Einverständnis zu geben und das Formular auszufüllen:</p>
-<p><a href="<?php echo esc_url(add_query_arg(array('id' => $application->application_id, 'key' => $review->filepath), home_url('/consent'))); ?>">Einverständniserklärung ausfüllen</a></p>
+<p><a href="<?php echo esc_url(add_query_arg(array('id' => $application->ID, 'key' => $review->filepath), home_url('/consent'))); ?>">Einverständniserklärung ausfüllen</a></p>
 <p>Viele Grüße,<br>Das Team von Commit IQ</p>
