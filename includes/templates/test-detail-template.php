dd7ceab59915cdf947015b9ff407bdfa0da146e2
jjaeger94 <janjaeger2020@gmail.com>
2024-04-24 16:51:45 +0200

    added book cover

--- includes/templates/test-detail-template.php
+++ includes/templates/test-detail-template.php
@@ -1,22 +1,36 @@
-<h1><?php echo $test->title; ?></h1>
+<div class="container">
+    <div class="row">
+        <!-- Titel und Affiliate-Link -->
+        <div class="col-md-9">
+            <h1><?php echo $test->title; ?></h1>
+            <p>Affiliate-Link: <a href="<?php echo esc_url($test->affiliate_link); ?>"><?php echo esc_url($test->affiliate_link); ?></a></p>
+        </div>
+        <!-- Bild -->
+        <div class="col-md-3 text-right">
+            <?php if (!empty($test->image_link)) : ?>
+                <img src="<?php echo esc_url($test->image_link); ?>" alt="Bild" style="max-height: 200px;">
+            <?php endif; ?>
+        </div>
+    </div>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Fragetext</th>
-            <th>Bearbeiten</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($questions as $question) : ?>
+    <table class="table">
+        <thead>
             <tr>
-                <td><?php echo $question->ID; ?></td>
-                <td><?php echo $question->question_text; ?></td>
-                <td><a href="<?php echo esc_url( home_url( '/frage-details/?tid=' . $test->ID . '&qid=' . $question->ID ) ); ?>" class="edit-question" data-question-id="<?php echo $question->ID; ?>">Bearbeiten</a></td>
+                <th>ID</th>
+                <th>Fragetext</th>
+                <th>Bearbeiten</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($questions as $question) : ?>
+                <tr>
+                    <td><?php echo $question->ID; ?></td>
+                    <td><?php echo $question->question_text; ?></td>
+                    <td><a href="<?php echo esc_url(home_url('/frage-details/?tid=' . $test->ID . '&qid=' . $question->ID)); ?>" class="edit-question" data-question-id="<?php echo $question->ID; ?>">Bearbeiten</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 
-<a href="<?php echo esc_url( home_url( '/frage-details/?tid=' . $test->ID ) ); ?>" id="add-question">Neue Frage hinzufügen</a>
+    <a href="<?php echo esc_url(home_url('/frage-details/?tid=' . $test->ID)); ?>" id="add-question">Neue Frage hinzufügen</a>
+</div>
