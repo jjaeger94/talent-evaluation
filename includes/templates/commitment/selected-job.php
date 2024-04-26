66719cf93a644b4223c42e5a704fed65f366dfea
jjaeger94 <janjaeger2020@gmail.com>
2024-04-26 11:28:34 +0200

    added datenschutz feld

--- includes/templates/commitment/selected-job.php
+++ includes/templates/commitment/selected-job.php
@@ -4,7 +4,7 @@
             <div class="card mt-5">
                 <div class="card-body">
                     <h5 class="card-title">Ihre Bewerbungsstelle</h5>
-                    <p class="card-text">Bitte bestätigen Sie die Stelle um fortzufahren.</p>
+                    <p class="card-text">Bitte bestätigen Sie die Stelle, um fortzufahren.</p>
                     <div class="row">
                         <div class="col-md-12">
                             <p><strong>Stellenbezeichnung:</strong></p>
@@ -12,6 +12,12 @@
                         </div>
                     </div>
                     <form method="get" action="<?php echo home_url('/test-methode/') ?>">
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" type="checkbox" value="" id="dataProcessingCheckbox" name="dataProcessingCheckbox" required>
+                            <label class="form-check-label" for="dataProcessingCheckbox">
+                                Ich stimme der Verarbeitung meiner Daten zu. <a href="/datenschutzerklaerung">Datenschutzerklärung</a>
+                            </label>
+                        </div>
                         <?php if(isset($aid)): ?>
                             <input type="hidden" name="aid" value="<?php echo esc_attr( $aid ); ?>">
                         <?php else: ?>
