6b2c7770792fb7329c2fd9ce51ba759098e2f1d3
jjaeger94 <janjaeger2020@gmail.com>
2024-05-08 10:37:21 +0200

    added margin to info box

--- includes/templates/chatbot/chatbot-info.php
+++ includes/templates/chatbot/chatbot-info.php
@@ -1,6 +1,6 @@
 <div class="alert alert-info w-100">
     <div class="row align-items-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-3">
             <!-- Text mit Informationen -->
             <p>Du arbeitest in einem Brillengeschäft. Eine Kundin betritt den Laden und sucht nach einer neuen Brille.</p>
             <p>Versuche, der Kundin eine Brille zu verkaufen. Du kannst dir die verfügbaren Brillen anzeigen lassen, indem du auf 'Brillen anzeigen' klickst.</p>
