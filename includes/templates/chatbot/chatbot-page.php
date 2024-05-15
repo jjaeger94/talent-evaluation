4798c060b2a27f473a99cf7604f6097ac88b4383
jjaeger94 <janjaeger2020@gmail.com>
2024-05-15 08:54:56 +0200

    added red glow

--- includes/templates/chatbot/chatbot-page.php
+++ includes/templates/chatbot/chatbot-page.php
@@ -25,7 +25,7 @@
 <div id="chat-input-container" class="fixed-bottom">
     <form id="chat-form" class="mt-4">
         <div class="input-group">
-            <input type="text" id="user-message-input" class="form-control" placeholder="Deine Nachricht...">
+            <input type="text" id="user-message-input" class="form-control" placeholder="Deine Nachricht..." required>
             <button id="button-send-message" type="submit" class="btn btn-primary">Senden</button>
         </div>
     </form>
@@ -47,51 +47,60 @@
             e.preventDefault(); 
             // Nachricht aus dem Eingabefeld abrufen
             var userMessage = $('#user-message-input').val();
-            $('#button-send-message').prop('disabled', true);
-            $('.message-container').last().append('<div class="message user">' + userMessage + '</div>');
-            // Nach dem Senden die Eingabe löschen
-            $('#user-message-input').val('');
-            $('#loading-indicator').show();
-            window.scrollTo(0, $('#loading-indicator').offset().top + $('#loading-indicator').height() - $(window).height() + 80);
+            var trimmed = userMessage.trim();
+            if(trimmed != ''){
+                $('#button-send-message').prop('disabled', true);
+                $('.message-container').last().append('<div class="message user">' + userMessage + '</div>');
+                // Nach dem Senden die Eingabe löschen
+                $('#user-message-input').val('');
+                $('#loading-indicator').show();
+                window.scrollTo(0, $('#loading-indicator').offset().top + $('#loading-indicator').height() - $(window).height() + 80);
 
-            // AJAX-Anfrage senden, um die Nachricht zu speichern
-            $.ajax({
-                type: 'POST',
-                url: '<?php echo admin_url('admin-ajax.php'); ?>',
-                data: {
-                    action: 'send_message',
-                    message: userMessage
-                },
-                success: function(response) {
-                    if(response.success){
-                        let data = JSON.parse(response.data);
-                        console.log(data);
-                        $('#loading-indicator').hide();
-                        if(data.state === 'success') {
-                            $('#chat-form').hide();
-                            $('.message-container').last().append('<div class="alert alert-info w-100">Danke fürs mitmachen! Du kannst das Fenster jetzt schließen</div>')
-                            $('#talentFormModal').show();
-                        } else if(data.state === 'failed') {
-                            $('#testResultMessage').text('Schade! Der Test wurde nicht bestanden.');
-                            $('#testResultModal').modal('show');
-                            $('#chat-form').hide();
-                            $('.message-container').last().append('<div class="alert alert-info w-100">Danke fürs mitmachen! Du kannst das Fenster jetzt schließen</div>');
-                        }else{
-                            // Erfolgreich: Neue Nachricht anzeigen
-                            $('.message-container').last().append('<div class="message assistant">' + data.message + '</div>');
-                            window.scrollTo(0, $('.message-container').offset().top + $('.message-container').height() - $(window).height() +50);
+                // AJAX-Anfrage senden, um die Nachricht zu speichern
+                $.ajax({
+                    type: 'POST',
+                    url: '<?php echo admin_url('admin-ajax.php'); ?>',
+                    data: {
+                        action: 'send_message',
+                        message: userMessage
+                    },
+                    success: function(response) {
+                        if(response.success){
+                            let data = JSON.parse(response.data);
+                            console.log(data);
+                            $('#loading-indicator').hide();
+                            if(data.state === 'success') {
+                                $('#chat-form').hide();
+                                $('.message-container').last().append('<div class="alert alert-info w-100">Danke fürs mitmachen! Du kannst das Fenster jetzt schließen</div>')
+                                $('#talentFormModal').show();
+                            } else if(data.state === 'failed') {
+                                $('#testResultMessage').text('Schade! Der Test wurde nicht bestanden.');
+                                $('#testResultModal').modal('show');
+                                $('#chat-form').hide();
+                                $('.message-container').last().append('<div class="alert alert-info w-100">Danke fürs mitmachen! Du kannst das Fenster jetzt schließen</div>');
+                            }else{
+                                // Erfolgreich: Neue Nachricht anzeigen
+                                $('.message-container').last().append('<div class="message assistant">' + data.message + '</div>');
+                                window.scrollTo(0, $('.message-container').offset().top + $('.message-container').height() - $(window).height() +50);
+                            }
                         }
+                    },
+                    error: function(xhr, status, error) {
+                        // Fehler: Fehlermeldung anzeigen oder Fehlerbehandlung durchführen
+                        console.error(error);
+                    },
+                    complete: function(){
+                        $('#loading-indicator').hide();
+                        $('#button-send-message').prop('disabled', false);
                     }
-                },
-                error: function(xhr, status, error) {
-                    // Fehler: Fehlermeldung anzeigen oder Fehlerbehandlung durchführen
-                    console.error(error);
-                },
-                complete: function(){
-                    $('#loading-indicator').hide();
-                    $('#button-send-message').prop('disabled', false);
-                }
-            });
+                });
+            }else{
+                // Eingabefeld hervorheben, wenn keine Nachricht eingegeben wurde
+                $('#user-message-input').addClass('input-error');
+                setTimeout(function() {
+                    $('#user-message-input').removeClass('input-error');
+                }, 1000);
+            }
         });
     });
 </script>
