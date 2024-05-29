d2aacbf62e45220f2e8ed89721118d96a5f0e802
jjaeger94 <janjaeger2020@gmail.com>
2024-05-29 09:35:54 +0200

    changed folder names

--- includes/details/talent-detail-template.php
+++ includes/details/talent-detail-template.php
@@ -1,7 +1,7 @@
 <?php if ($talent) : ?>
     <div class="container">
         <?php if (current_user_can('dienstleister')) : ?>
-        <?php include TE_DIR.'talents/meta.php'; ?>
+        <?php include TE_DIR.'controls/talent.php'; ?>
         <button class="btn btn-primary mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#personalDataCollapse" aria-expanded="true" aria-controls="personalDataCollapse">
             Alle Infos anzeigen
         </button>
@@ -13,21 +13,21 @@
         </button>
         <div class="collapse" id="personalDataCollapse">
         <?php else: ?>
-        <?php include TE_DIR.'talents/info.php'; ?>
+        <?php include TE_DIR.'profile/info.php'; ?>
         <?php endif; ?>
-        <?php include TE_DIR.'talents/personal-data-start.php'; ?>
-        <?php include TE_DIR.'talents/apprenticeship.php'; ?>
-        <?php include TE_DIR.'talents/studies.php'; ?>
-        <?php include TE_DIR.'talents/experience.php'; ?>
-        <?php include TE_DIR.'talents/eq.php'; ?>
-        <?php include TE_DIR.'talents/personal-data-end.php'; ?>
+        <?php include TE_DIR.'profile/personal-data-start.php'; ?>
+        <?php include TE_DIR.'profile/apprenticeship.php'; ?>
+        <?php include TE_DIR.'profile/studies.php'; ?>
+        <?php include TE_DIR.'profile/experience.php'; ?>
+        <?php include TE_DIR.'profile/eq.php'; ?>
+        <?php include TE_DIR.'profile/personal-data-end.php'; ?>
         <?php if (current_user_can('dienstleister')) : ?>
             </div>
             <div class="collapse" id="jobsTableCollapse">
                 <?php include TE_DIR.'tables/talent-jobs-table-template.php'; ?>
             </div>
             <div class="collapse" id="chatCollapse">
-                <?php include TE_DIR.'talents/chat.php'; ?>
+                <?php include TE_DIR.'controls/chat.php'; ?>
             </div>
         <?php endif; ?>
     </div>
