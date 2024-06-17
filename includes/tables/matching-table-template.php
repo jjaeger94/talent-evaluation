c38a867554e09fe536313c0145e197d4e80f295b
jjaeger94 <janjaeger2020@gmail.com>
2024-06-17 15:24:48 +0200

    added links

--- includes/tables/matching-table-template.php
+++ includes/tables/matching-table-template.php
@@ -6,14 +6,22 @@
                 <th>Talent</th>
                 <th>bearbeitet</th>
                 <th>Status</th>
-                <th>Talentdetails</th>
+                <th>Matchingdetails</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($matchings as $match) : ?>
                 <tr>
-                    <td><?php echo esc_html($match->job_title); ?></td>
-                    <td><?php echo esc_html($match->prename) .' '.esc_html($match->surname); ?></td>
+                    <td>
+                        <a href="<?php echo esc_url(home_url('/job-details/?id=' . $match->job_id)); ?>">
+                            <?php echo esc_html($match->job_title); ?>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="<?php echo esc_url(home_url('/talent-details/?id=' . $match->talent_id)); ?>">
+                            <?php echo esc_html($match->prename) .' '.esc_html($match->surname); ?>
+                        </a>
+                    </td>
                     <td><?php echo date('d.m.Y', strtotime($match->edited)); ?></td>
                     <td><?php echo esc_html(get_matching_state($match->value)); ?></td>
                     <td><a href="<?php echo esc_url(home_url('/compare-details/?talent_id=' . $match->talent_id. '&job_id='. $match->job_id)); ?>">Details</a></td>
