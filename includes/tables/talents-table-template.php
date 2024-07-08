b47d8ae775eaa9ff735ab634f8319e61685598e6
jjaeger94 <janjaeger2020@gmail.com>
2024-07-08 14:46:16 +0200

    added name column

--- includes/tables/talents-table-template.php
+++ includes/tables/talents-table-template.php
@@ -2,11 +2,11 @@
     <table class="table table-striped">
         <thead>
             <tr>
+                <th>Name</th>
                 <th>E-Mail</th>
                 <th>Telefonnummer</th>
                 <th>PLZ</th>
                 <th>Ref</th>
-                <th>Teilzeit</th>
                 <th>Status</th>
                 <th>Talentdetails</th>
             </tr>
@@ -14,11 +14,11 @@
         <tbody>
             <?php foreach ($talents as $talent) : ?>
                 <tr>
+                    <td><?php echo esc_html($talent->prename) . ' ' . esc_html($talent->surname); ?></td>
                     <td><?php echo esc_html($talent->email); ?></td>
                     <td><?php echo esc_html($talent->mobile); ?></td>
                     <td><?php echo esc_html($talent->post_code); ?></td>
                     <td><?php echo esc_html($talent->ref); ?></td>
-                    <td><?php echo $talent->part_time ? 'Ja' : 'Nein'; ?></td>
                     <td><?php echo esc_html(get_talent_state($talent)); ?></td>
                     <td><a href="<?php echo esc_url(home_url('/talent-details/?id=' . $talent->ID)); ?>">Details</a></td>
                 </tr>
