3961effdb85132cc0ca897f4e9e13e5dd5ef31bf
jjaeger94 <janjaeger2020@gmail.com>
2024-07-09 16:55:16 +0200

    added conatct page to menu

--- includes/menu/entries.php
+++ includes/menu/entries.php
@@ -2,4 +2,5 @@
     <li><a href="<?php echo home_url( '/profile' ); ?>">Mein Profil</a></li>
     <!-- <li><a href="<?php echo home_url( '/matching' ); ?>">Stellen</a></li> -->
     <li><a href="<?php echo home_url( '/preferences' ); ?>">Präferenzen</a></li>
+    <li><a href="<?php echo home_url( '/contact' ); ?>">Kontakt</a></li>
 </div>
