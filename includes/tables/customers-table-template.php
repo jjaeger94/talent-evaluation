25e39323d4840d58ef4a39a76d16fbeaa85a51c2
jjaeger94 <janjaeger2020@gmail.com>
2024-07-10 15:14:11 +0200

    make customer name clickable

--- includes/tables/customers-table-template.php
+++ includes/tables/customers-table-template.php
@@ -2,19 +2,15 @@
     <table class="table table-striped">
         <thead>
             <tr>
-                <th>ID</th>
                 <th>Firmenname</th>
                 <th>Status</th>
-                <th>Details</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($customers as $customer) : ?>
                 <tr>
-                    <td><?php echo $customer->ID; ?></td>
-                    <td><?php echo $customer->company_name; ?></td>
+                    <td><a href="<?php echo esc_url(home_url('/customer-details/?id=' . $customer->ID)); ?>"><?php echo $customer->company_name; ?></a></td>
                     <td><?php echo get_customer_state($customer->state); ?></td>
-                    <td><a href="<?php echo esc_url(home_url('/customer-details/?id=' . $customer->ID)); ?>">Details</a></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
