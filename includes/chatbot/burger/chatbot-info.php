e488f178bc92584e082e5108358bdbaf4e58d09a
jjaeger94 <janjaeger2020@gmail.com>
2024-07-12 11:24:53 +0200

    better texts

--- includes/chatbot/burger/chatbot-info.php
+++ includes/chatbot/burger/chatbot-info.php
@@ -31,7 +31,10 @@
                 <ul>
                     <li>Stelle viele Fragen, um Dieters Perspektive und Bedürfnisse zu verstehen.</li>
                     <li>Nutze deine Empathie, um eine unterstützende Atmosphäre zu schaffen.</li>
+                    <li>Keine Lust zu tippen? Nutze einfach die Spracheingabe, indem du das Mikrofon-Symbol gedrückt hältst.</li>
+                    <li>Wenn du über Instagram das Mikrofon benutzen möchtest, tippe oben rechts auf die drei Punkte und wähle 'In externem Browser öffnen'.</li>
                 </ul>
+                <br>
                 <p>Das Spiel beginnt mit deiner ersten Nachricht. Viel Erfolg!</p>
             </div>
             <div class="modal-footer">
