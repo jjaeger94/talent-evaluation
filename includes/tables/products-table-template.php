06d946653fff60b1e0336e3729e49540c4100a78
jjaeger94 <janjaeger2020@gmail.com>
2024-07-17 15:03:19 +0200

    added eigenes and konkurrenz

--- includes/tables/products-table-template.php
+++ includes/tables/products-table-template.php
@@ -9,7 +9,7 @@
         <tbody>
             <?php foreach ($products as $product) : ?>
                 <tr>
-                    <td><a href="<?php echo esc_url(home_url('/product-details/?id=' . $product->ID)); ?>"><?php echo $product->product_name; ?></a></td>
+                    <td><a href="<?php echo esc_url(home_url('/product-details/?id=' . $product->ID)); ?>"><?php echo $product->product_name .' ('. get_product_type($product->type) . ')'; ?></a></td>
                     <td><?php echo $product->product_description; ?></td>
                 </tr>
             <?php endforeach; ?>
