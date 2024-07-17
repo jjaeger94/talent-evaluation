389630d75ec719b1893350e7d010eaf41e3c408c
jjaeger94 <janjaeger2020@gmail.com>
2024-07-17 15:44:19 +0200

    added Categories

--- includes/chatbot/product-view.php
+++ includes/chatbot/product-view.php
@@ -1,10 +1,21 @@
 <?php 
 $products = get_products_for_game_id($game->ID);
+
 if (!empty($products)) :
+    // Filter products by type
+    $own_products = array_filter($products, function($product) {
+        return $product->type == 0;
+    });
+    $competitor_products = array_filter($products, function($product) {
+        return $product->type == 1;
+    });
 ?>
-<h5>Produkte:</h5>
+
+
+<?php if (!empty($own_products)) : ?>
+<h5>Eigene Produkte:</h5>
 <div class="row row-cols-1 row-cols-md-3 g-4">
-    <?php foreach ($products as $product) : ?>
+    <?php foreach ($own_products as $product) : ?>
         <div class="col">
             <div class="card h-100">
                 <img src="<?php echo $product->image_url; ?>" class="card-img-top">
@@ -15,5 +26,23 @@ if (!empty($products)) :
             </div>
         </div>
     <?php endforeach; ?> 
-</div>
+    </div>
+<?php endif; ?>
+<br>
+<?php if (!empty($competitor_products)) : ?>
+<h5>Konkurrenzprodukte:</h5>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <?php foreach ($competitor_products as $product) : ?>
+        <div class="col">
+            <div class="card h-100">
+                <img src="<?php echo $product->image_url; ?>" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $product->product_name; ?></h5>
+                    <p class="card-text"><?php echo $product->product_description; ?></p>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?> 
+    </div>
+<?php endif; ?>
 <?php endif; ?>
