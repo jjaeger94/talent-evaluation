3c8b4167e2587a5f25600df5349cf9ccfda5255f
jjaeger94 <janjaeger2020@gmail.com>
2024-07-19 10:22:00 +0200

    uncomment preferences

--- includes/menu/entries.php
+++ includes/menu/entries.php
@@ -1,6 +1,6 @@
 <div class="menu">
     <li><a href="<?php echo home_url( '/profile' ); ?>">Mein Profil</a></li>
     <!-- <li><a href="<?php echo home_url( '/matching' ); ?>">Stellen</a></li> -->
-    <li><a href="<?php echo home_url( '/preferences' ); ?>">Präferenzen</a></li>
+    <!-- <li><a href="<?php echo home_url( '/preferences' ); ?>">Präferenzen</a></li> -->
     <li><a href="<?php echo home_url( '/contact' ); ?>">Kontakt</a></li>
 </div>
